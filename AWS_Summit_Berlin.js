80a15cf1f446b2c78a8ed7ee7500274b498fe731
adaydan <66020039+adaydan@users.noreply.github.com>
2023-04-12 14:43:03 -0700

    Update AWS_Summit_Berlin.js

--- AWS_Summit_Berlin.js
+++ AWS_Summit_Berlin.js
@@ -1,28 +1,3 @@
 (function () {
-    MktoForms2.whenReady(function (form) {
-        const oldElement = document.querySelector('.mktoRadioList');
-        const newHtml = `
-            <input name="eventRegType" id="mktoRadio_24223_0" value="Berlin" aria-required="true"
-                aria-labelledby="LbleventRegType LblmktoRadio_24223_0 InstructeventRegType" type="radio" class="mktoField">
-            <label for="mktoRadio_24223_0" id="LblmktoRadio_24223_0">Berlin</label>
-
-            <input name="eventRegType" id="mktoRadio_24223_1" value="Washington DC" aria-required="true"
-                aria-labelledby="LbleventRegType LblmktoRadio_24223_1 InstructeventRegType" type="radio" class="mktoField">
-            <label for="mktoRadio_24223_1" id="LblmktoRadio_24223_1">Washington DC</label>
-
-            <input name="eventRegType" id="mktoRadio_24223_2" value="NYC" aria-required="true"
-                aria-labelledby="LbleventRegType LblmktoRadio_24223_2 InstructeventRegType" type="radio" class="mktoField">
-            <label for="mktoRadio_24223_2" id="LblmktoRadio_24223_2">NYC</label>
-
-            <input name="eventRegType" id="mktoRadio_24223_3" value="London" aria-required="true"
-                aria-labelledby="LbleventRegType LblmktoRadio_24223_3 InstructeventRegType" type="radio" class="mktoField">
-            <label for="mktoRadio_24223_3" id="LblmktoRadio_24223_3">London</label>
-        `;
-
-        if (oldElement) {
-            oldElement.innerHTML = newHtml.trim();
-        } else {
-            console.error('Element with class "mktoRadioList" not found')
-        }
-    });
+console.log('hello');
 })();
